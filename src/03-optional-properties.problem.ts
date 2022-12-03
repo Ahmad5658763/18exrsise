1365e75e908ae7eab92c11911ed92da82c09e6ea
Ahmad5658763 <114980901+Ahmad5658763@users.noreply.github.com>
2022-12-03 12:57:51 +0300

    Add files via upload

---
+++ src/03-optional-properties.problem.ts
@@ -0,0 +1,35 @@
+import { expect, it } from "vitest";
+
+export const getName = (params: { first: string; last: string }) => {
+  if (params.last) {
+    return `${params.first} ${params.last}`;
+  }
+  return params.first;
+};
+
+it("Should work with just the first name", () => {
+  const name = getName({
+      _first: newFunction(),
+      get first() {
+          return this._first;
+      },
+      set first(value) {
+          this._first = value;
+      },
+  });
+
+  expect(name).toEqual("Matt");
+});
+
+it("Should work with the first and last name", () => {
+  const name = getName({
+    first: "Matt",
+    last: "Pocock",
+  });
+
+  expect(name).toEqual("Matt Pocock");
+});
+
+function newFunction(): string {
+    return "Matt";
+}
